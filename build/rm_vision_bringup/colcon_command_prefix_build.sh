b5d2a050fc50a4e80f84eb9e2a619a77a269b483
YiMIng <105438207+pym96@users.noreply.github.com>
2024-11-16 16:49:54 +0800

    Delete build directory

--- build/rm_vision_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/dan/MA/CJ/install/auto_aim_interfaces/share/auto_aim_interfaces/package.sh"
-. "/home/dan/MA/CJ/install/rm_serial_driver/share/rm_serial_driver/package.sh"
